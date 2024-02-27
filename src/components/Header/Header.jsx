9c5c367417289c0dfb25bab2ca043fdaa22d0b9d
Dmitriy Zakharov <dzaharov794@gmail.com>
2024-02-27 09:39:16 +0300

    add comopnents Header and Container

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,53 @@
+import Container from './UI/Container';
+
+import styles from './Header.module.css';
+
+import logo from '../../assets/logo.png';
+import phoneIcon from '../../assets/phone-icon.svg';
+import searchIcon from '../../assets/search-icon.svg';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Container>
+        <div className={styles.header__inner}>
+          <div className={styles.header__logo_inner}>
+            <img src={logo} alt="Логотип компании Orange" />
+            <p className={styles.header__logo_text}>
+              Разработано в Германии. Сделано в России.
+            </p>
+          </div>
+          <div className={styles.header__search_inner}>
+            <div className={styles.header__phone_inner}>
+              <img src={phoneIcon} alt="Телефон" />
+              <a className={styles.header__phone} href="tel:+79055432345">
+                +7 (905) 543 23 45
+              </a>
+            </div>
+            <button type="button" className={styles.header__search_button}>
+              <img src={searchIcon} alt="Поиск" />
+            </button>
+          </div>
+        </div>
+        <div className={styles.header__description_block}>
+          <h1>Сантехника и мебель для ванной комнаты</h1>
+          <nav className={styles.header__navigation}>
+            <ul className={styles.header__nav_list}>
+              <li>
+                <a className={styles.header__nav_link} href="#company">О компании</a>
+              </li>
+              <li>
+                <a className={styles.header__nav_link} href="#products">О продукции</a>
+              </li>
+              <li>
+                <a className={styles.header__nav_link} href="#catalog">Каталог товаров</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
